5e9f24251ee584b271350f120eeb92336adcda43
Eilaf Aljundi <elafjundi@gmail.com>
2023-01-09 19:55:37 -0500

    base commands

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const dotenv = require('dotenv');
+dotenv.config();
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
+
